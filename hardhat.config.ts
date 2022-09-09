61b463f1f3ece0eae5ea176c2c3c2692603377e4
Piwo96 <philipp.rollmann@online.de>
2022-09-10 00:45:18 +0200

    added configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: {
+        compilers: [
+            { version: "0.8.9" },
+            { version: "0.8.4" },
+            { version: "0.8.0" },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            chainId: 31337,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 500000,
+    },
 };
 
 export default config;
